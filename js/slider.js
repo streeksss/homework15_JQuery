8b1209f751c1be24691142a9353ced968886d862
streeksss <streeksmail@gmail.com>
2016-02-01 02:18:26 +0200

    first commit

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+// Can also be used with $(document).ready()
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    controlsContainer: $(".custom-controls-container"),
+    customDirectionNav: $(".custom-navigation a")
+  });
+});
